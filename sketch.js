08b46bdced2e5bf9fcb317b905dd8c560c968d61
shreshthshukla <74090495+shreshthshukla@users.noreply.github.com>
2021-09-12 22:20:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,16 +3,30 @@ block18, block19, block20, block21, block22, block23, block24, block25, block26,
 block34, block35, block36, block37, block38, block39, block40, block41, block42, block43, block44, block45, block46, block47, block48, block49, 
 block50;
 
+var homeI;
 var paddle, ball;
 var ball1I, ball2I;
-var level1I, level2I, level3I, level4I, level5I;
 var padI1, padI2;
-
-var gameState = 0;
-var ready = 0;
-var serve = 1;
-var play = 2;
-var end = 3;
+var score, scoreI;
+var gameState = -3;
+var loading = -3;
+var home = -2;
+var levels = -1;
+var serve = 0;
+var play = 1;
+var end = 2;
+var lifeline;
+var level1, level2, level3;
+var level1I, level2I, level3I;
+var boun;
+var ani;
+var start, startI;
+var playI, pauseI, buttonP;
+var TT = 150;
+var load, loadI; 
+var lock1, lock2, lockI;
+var levelsI;
+var score2;
 
 function preload(){
   padI1 = loadImage("Paddle1.png");
@@ -20,13 +34,252 @@ function preload(){
 
   ball1I = loadImage("Ball.png");
   ball2I = loadImage("Fire ball.png");
+
+  ani = loadAnimation("1.jpeg","2.jpeg","3.jpeg","4.jpeg","5.jpeg","6.jpeg","7.jpeg","8.jpeg","9.jpeg","10.jpeg","11.jpeg","12.jpeg","13.jpeg",
+  "14.jpeg","15.jpeg","16.jpeg","17.jpeg","18.jpeg");
+
+  startI = loadImage("start.png");
+
+  playI = loadImage("Play.png");
+  
+  pauseI = loadImage("Pause.png");
+
+  loadI = loadAnimation("Load1.png","Load2.png","Load3.png","Load4.png","Load5.png","Load6.png","Load7.png","Load8.png","Load9.png","Load10.png",
+  "Load11.png","Load12.png");
+
+  level1I = loadImage("l1.png");
+  level2I = loadImage("l2.png");
+  level3I = loadImage("l3.png");
+
+  lockI = loadImage("Lock.png");
+
+  levelsI = loadImage("Levels.png");
 }
 
 function setup() {
-createCanvas(1500,750);
+createCanvas(1520,windowHeight);
 
+start = createSprite(750,600);
+start.addImage(startI)
+start.scale = 0.2
+start.setCollider("circle",0,0,300)
+
+load = createSprite(750,320);
+load.addAnimation("changing",loadI);
+
+// animation
+boun = createSprite(750,300);
+boun.addAnimation("moving",ani);
+boun.scale = 0.5
+boun.visible = false;
+
+//Creating edges
 edges = createEdgeSprites();
 
+// creating the player paddle.
+paddle = createSprite(750,700)
+paddle.addImage(padI1);
+paddle.scale=0.8
+paddle.setCollider("rectangle",-2,0,210,35)
+paddle.visible = false;
+
+// creating ball
+ball = createSprite(750,paddle.y-40)
+ball.addImage(ball1I);
+ball.scale=0.12
+ball.setCollider("circle",0,0,190);
+ball.visible = false;
+
+level1 = createSprite(200,315)
+level1.addImage(level1I);
+level1.scale = 0.5
+level1.visible = false;
+
+level2 = createSprite(750,315)
+level2.addImage(level2I);
+level2.scale = 0.5
+level2.visible = false;
+
+level3 = createSprite(1300,315)
+level3.addImage(level3I);
+level3.scale = 0.5
+level3.visible = false;
+
+lock1 = createSprite(810,370)
+lock1.addImage(lockI);
+lock1.scale = 0.1
+lock1.visible = false;
+
+lock2 = createSprite(1360,370)
+lock2.addImage(lockI);
+lock2.scale = 0.1
+lock2.visible = false;
+
+}
+
+function draw() {
+  background("lightblue");  
+  drawSprites();
+
+
+  // Making the ball bounce off the edges and paddle
+   ball.bounceOff(paddle);
+   ball.bounceOff(edges[0]);
+   ball.bounceOff(edges[1]);
+   ball.bounceOff(edges[2]);
+
+  if(gameState==-3){
+    textFont("Algerian")
+    strokeWeight(20);
+    textSize(20);
+    fill ("black")
+    text("LOADING...",20,50);
+    TT--;
+    start.visible = false;
+  }
+  if(TT==0&&gameState==-3){
+    gameState = -2
+  }
+
+  if(gameState==-2){
+
+    start.visible = true;
+
+    boun.visible = true;
+
+    load.visible = false;
+
+    fill("Black")
+    textSize(60);
+    text("𝓑𝓻𝓲𝓬𝓴𝓼 𝓑𝓵𝓪𝓼𝓽",540,100);
+
+    if(mousePressedOver(start)&&gameState==-2){
+      gameState = -1
+    }
+  }
+  if(gameState==-1){
+
+    start.visible = false;
+
+    boun.visible = false;
+
+    level1.visible=true;
+    level2.visible=true;
+    level3.visible=true;
+    lock1.visible=true;
+    lock2=visible=true;
+
+   if(gameState==-1&&mousePressedOver(level1)){
+    gameState = 0
+   }
+  }
+
+  if(gameState==0){
+    level1.visible=false;
+    level2.visible=false;
+    level3.visible=false;
+    lock1.visible=false;
+    lock2=visible=false;
+
+    bricks();
+
+    paddle.visible = true;
+    ball.visible = true;
+
+    if(keyDown("space")&&gameState==0){
+      gameState = 1;
+      ballServe();
+    }
+
+  }
+
+  if(gameState==1){
+  // bounce the paddle with the edges
+  paddle.bounceOff(edges[0])
+  paddle.bounceOff(edges[1])
+
+  // Making the paddle move with keys.
+  if(keyDown("right")){
+  paddle.x+=13
+  }
+  if(keyDown("left")){
+  paddle.x-=13
+  }
+
+  moveBricks();
+
+  }
+
+
+
+
+
+
+
+    
+  
+
+  // if(keyDown("r")){
+  //   resetAll();
+  // }
+ 
+  text(mouseX+","+mouseY,100,height/1.5)
+}
+
+function ballServe(){
+  ball.velocityX=Math.round(random(-10,10))
+  ball.velocityY=-10
+}
+
+function resetAll(){
+block1.x=95
+block2.x=265;
+block3.x=445;
+block4.x=630;
+block5.x=825;
+block6.x=1020
+block7.x=1220;
+block8.x=1420;
+block9.x=1420;
+block10.x=80;
+block11.x=230;
+block12.x=390;
+block13.x=570;
+block14.x=780;
+block15.x=990;
+block16.x=1190;
+block17.x=1405;
+block18.x=1230;
+block19.x=110;
+block20.x=310;
+block21.x=490;
+block22.x=640;
+block23.x=830;
+block24.x=1045;
+block34.x=80;
+block35.x=245;
+block36.x=445
+block37.x=633
+block38.x=820
+block39.x=1015
+block40.x=1180
+block41.x=1350
+block42.x=1475;
+block43.x=100
+block44.x=275
+block45.x=465
+block46.x=675;
+block47.x=870
+block48.x=1075
+block49.x=1280;
+block50.x=1440;
+
+paddle.x = 750
+}
+
+function bricks(){
+
+//Creating the Bricks.
 block1 = createSprite(95,60,150,30);
 block1.shapeColor="red";
 block2 = createSprite(265,60,140,30);
@@ -45,7 +298,7 @@ block8 = createSprite(1420,60,160,30);
 block8.shapeColor="magenta";
 
 block9 = createSprite(1420,160,160,30);
-block9.shapeColor="lightblue";
+block9.shapeColor="red";
 
 block10 = createSprite(80,110,120,30);
 block10.shapeColor="orange";
@@ -65,7 +318,7 @@ block17 = createSprite(1405,110,190,30);
 block17.shapeColor="blue";
 
 block18 = createSprite(1230,160,150,30);
-block18.shapeColor="lightblue";
+block18.shapeColor="pink";
 
 block19 = createSprite(110,160,180,30);
 block19.shapeColor="orange";
@@ -80,25 +333,12 @@ block23.shapeColor="purple";
 block24 = createSprite(1045,160,175,30);
 block24.shapeColor="red";
 
-block25 = createSprite(80,210,120,30);
-block25.shapeColor="green";
-block26 = createSprite(245,210,160,30);
-block26.shapeColor="pink";
-block27 = createSprite(1045,210,175,30);
-block27.shapeColor="Air Force blue";
-block28 = createSprite(1045,210,175,30);
-block28.shapeColor="Almond";
-block29 = createSprite(1045,210,175,30);
-block29.shapeColor="Alizarin crimson";
-block30 = createSprite(1045,210,175,30);
-block30.shapeColor="amber";
-
 block34 = createSprite(80,210,120,30);
 block34.shapeColor="yellow";
 block35 = createSprite(245,210,160,30);
 block35.shapeColor="red";
 block36 = createSprite(445,210,190,30);
-block36.shapeColor="lightblue";
+block36.shapeColor="brown";
 block37 = createSprite(633,210,135,30);
 block37.shapeColor="blue";
 block38 = createSprite(820,210,190,30);
@@ -128,36 +368,9 @@ block49 = createSprite(1280,260,160,30);
 block49.shapeColor="tomato";
 block50 = createSprite(1440,260,115,30);
 block50.shapeColor="saddlebrown";
-
-// creating the player paddle.
-paddle = createSprite(width/2,700)
-paddle.addImage(padI1);
-paddle.scale=0.8
-// paddle.debug = true
-paddle.setCollider("rectangle",0,0,200,35)
-
-// creating ball
-ball = createSprite(width/2,paddle.y-40)
-ball.addImage(ball1I);
-ball.scale=0.12
-// ball.debug=true
-ball.setCollider("circle",0,0,190)
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
-
-  ball.bounceOff(paddle);
-  ball.bounceOff(edges[0])
-  ball.bounceOff(edges[1])
-  if(keyDown("right")){
-    paddle.x+=10
-  }
-  if(keyDown("left")){
-    paddle.x-=10
-  }
-
+function moveBricks(){
   if(ball.isTouching(block1)){ 
 
     ball.bounceOff(block1); 
@@ -348,54 +561,6 @@ function draw() {
 
   } 
 
-  if(ball.isTouching(block25)){ 
-
-    ball.bounceOff(block25); 
-
-    block25.x = 2000; 
-
-  } 
-
-  if(ball.isTouching(block26)){ 
-
-    ball.bounceOff(block26); 
-
-    block26.x = 2000; 
-
-  } 
-
-  if(ball.isTouching(block27)){ 
-
-    ball.bounceOff(block27); 
-
-    block27.x = 2000; 
-
-  }if(ball.isTouching(block28)){ 
-
-    ball.bounceOff(block28); 
-
-    block28.x = 2000; 
-
-  } 
-
-  if(ball.isTouching(block29)){ 
-
-    ball.bounceOff(block29); 
-
-    block29.x = 2000; 
-
-  } 
-
-  if(ball.isTouching(block30)){ 
-
-    ball.bounceOff(block30); 
-
-    block30.x = 2000; 
-
-  } 
-
-   
-
   if(ball.isTouching(block34)){ 
 
     ball.bounceOff(block34); 
@@ -531,13 +696,4 @@ function draw() {
     block50.x = 2000; 
 
   } 
-
- 
-
-  if(keyDown("space")){
-    ball.velocityX=Math.round(random(-10,10))
-    ball.velocityY=-10
-
-  }
-  text(mouseX+","+mouseY,width/2,height/1.5)
 }
